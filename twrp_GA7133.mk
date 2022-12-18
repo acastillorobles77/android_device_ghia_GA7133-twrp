2fa9fa21b088d96df83bd6234b60628cb65e9690
Angel Castillo <acastillorobles77@gmail.com>
2022-12-18 15:21:12 -0600

    GA7133: Update some files

--- twrp_GA7133.mk
+++ twrp_GA7133.mk
@@ -8,7 +8,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from GA7133 device
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_GA7133-user
-add_lunch_combo omni_GA7133-userdebug
-add_lunch_combo omni_GA7133-eng
+add_lunch_combo twrp_GA7133-user
+add_lunch_combo twrp_GA7133-userdebug
+add_lunch_combo twrp_GA7133-eng
