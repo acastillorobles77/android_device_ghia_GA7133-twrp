561b3f12269f4f3c2ff3804e245b260e4b7d32be
Angel Castillo <acastillorobles77@gmail.com>
2022-12-18 18:30:25 -0600

    GA7133: Drop obsolete dependencies

--- device.mk
+++ device.mk
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.ceres
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL += \
-    bootctrl.ceres \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
